46489b6cfa11acd2f7c776502192f6eddd6de75c
grace <123graciev@graces-MacBook-Pro.local>
2020-03-01 11:58:52 -0500

    trying again

--- js/appcf.js
+++ js/appcf.js
@@ -1,20 +1,7 @@
-const grid = [
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{},{},{},{}],
-  [{},{},{},{color:"red"},{},{},{}],
-];
- const render ()->{
-   const svg = document.getElementById("svg");
-   let doc = '';
-   for (var i = 0; i < grid.length; i++){
-     var row = grid[i];
-     for (var j = 0; j < row.length, j++){
-       doc = doc + '<circle fill= 'black' r= '1' x=${j}px y=$(i)px'> </circle>;
-     }
-   }
-   svg.innerHTML = doc;
- };
-
-render();
+ const grid = [
+   [{},{},{},{},{},{},{}],
+   [{},{},{},{},{},{},{}],
+   [{},{},{},{},{},{},{}],
+   [{},{},{},{},{},{},{}],
+   [{},{},{},{},{},{},{}],
+ ];
