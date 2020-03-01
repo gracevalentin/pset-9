85ea8f4a3e59805fc1e652983834042bdbdddfd2
grace <123graciev@graces-MacBook-Pro.local>
2020-03-01 12:57:10 -0500

    epic

--- js/appcf.js
+++ js/appcf.js
@@ -22,4 +22,44 @@ const render = ()=>{
      const square = grid[i][j];
      const color = square && square.color || 'gray';
 
- render();
+      doc = doc + `<circle onclick="clickSquare(${j},${i})" fill='${color}' r='30px' cx='${j * 70 + 50}px' cy='${i * 70 + 50}px'></circle>`;
+
+    }
+  }
+  svg.innerHTML = doc;
+};
+
+let currentColor = 'red';
+window.clickSquare = (x,y)=>{
+  if (!gameInPlay) {
+    return;
+  }
+   for (var i = grid.length - 1; i >= 0; i--) {
+    var row = grid[i];
+    var targetPlace = row[x];
+     if (!targetPlace.color) {
+       row[x] = {color:currentColor};
+       currentColor = currentColor === "red" ? "black" : "red";
+       render();
+       calculateWinners();
+       return;
+    }
+  }
+}
+
+function calculateWinners(){
+  console.log("Is there a winner?");
+  for (var i = 0; i < grid.length; i++) {
+    var row = grid[i];
+     for (var j = 0; j < row.length; j++) {
+       var square =  grid[i][j];
+       if (square && square.color) {
+         if (i === 0 || i === 1) {
+           if (grid[i + 1][j].color === square.color &&
+              grid[i + 2][j].color === square.color &&
+              grid[i + 3][j].color === square.color) {
+                aColorWins(square.color);
+
+                return;
+            }
+          }
